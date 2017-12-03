6639be97e8013a5c3fcb01f89ba846cccad54419
Yingjin Chen <yingjincs@gmail.com>
2017-12-03 11:48:16 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '481240032260114',
+        clientSecret: 'd0ad5875e52f39acb37110602aad02c4'
+    }
 }
